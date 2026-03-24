5c3d285d82e7bd3512779e60c4b4f80993d52e31
galleriadelpremio <erika.vecchietti@comune.suzzara.mn.it>
2026-03-24 09:04:23 +0100

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -6,7 +6,10 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./galleria.db")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
